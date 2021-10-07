c80e09ae0de1e23b08842d57b635287acd363c91
madhuriprasad <madhuri4434@gmail.com>
2021-10-07 01:13:19 -0400

    part7 @ 25

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,8 +1,8 @@
 from flaskblog.models import User, Post
-from flask import render_template, url_for, flash, redirect
-from flaskblog.forms import RegistrationForm, LoginForm
+from flask import render_template, url_for, flash, redirect, request
+from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm
 from flaskblog import app, db, bcrypt
-from flask_login import login_user, current_user, logout_user
+from flask_login import login_user, current_user, logout_user, login_required
 
 posts=[
     {
@@ -52,9 +52,10 @@ def login():
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(email= form.email.data).first()
-        if user and bcrypt.check_password_hash():
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
             login_user(user,remember= form.remember.data)
-            return redirect(url_for('home'))
+            next_page = request.args.get('next')
+            return redirect(next_page)if next_page else redirect(url_for('home'))
         else:
             flash('Login Unsuccessful.Please try again','danger')
     return render_template('login.html',title= 'Login', form= form)
@@ -64,9 +65,23 @@ def logout():
     logout_user()
     return redirect(url_for('home'))
 
-@app.route("/account")
+@app.route("/account", methods = ['GET','POST'])
+@login_required
 def account():
-    return render_template('account.html', title='Account')
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash('your account has been updated','success')
+        return redirect(url_for('account'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+
+    image_file = url_for('static',filename = 'profile_pics/' + current_user.image_file)
+    return render_template('account.html', title='Account',
+                           image_file = image_file, form = form)
 
 
 
